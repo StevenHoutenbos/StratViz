bf9b545c1ecfdf963f9f74882bc7ad5813961394
Steven <steven@houtenbos.nl>
2022-06-10 15:22:37 +0200

    Added grid layout for sensors and their options

--- client/src/components/PlotContent.js
+++ client/src/components/PlotContent.js
@@ -1,22 +1,16 @@
 import React, { useState } from "react";
 import { Button } from "@mui/material";
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { ButtonGroup } from "@mui/material";
-import { teal } from "@mui/material/colors";
-
-const primary = teal[500];
+import RemoveIcon from '@mui/icons-material/Remove';
 
 function PlotContent(props){
 
     const [open, setOpen] = React.useState(false);
     const [selectedBtn, setSelectedBtn] = React.useState(1);
-
-    const activeColor = teal[500]
 
     const handleClickOpen = () => {
       setOpen(true);
@@ -32,17 +26,34 @@ function PlotContent(props){
                 <Button variant="outlined" onClick={handleClickOpen}>
                     {props.plotName}
                 </Button>
-                <Dialog open={open} onClose={handleClose}>
+                <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth={"md"}>
                     <DialogTitle>{props.plotName}</DialogTitle>
                     <DialogContent>
                         <ButtonGroup disableElevation variant="contained" color="primary">
-                            <Button color={selectedBtn === 1 ?  "secondary" : "primary"} onClick={()=>setSelectedBtn(1)}>Lux</Button>
+                            <Button color={selectedBtn === 1 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(1)}>Lux</Button>
                             <Button color={selectedBtn === 2 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(2)}>Stella</Button>
                             <Button color={selectedBtn === 3 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(3)}>Vie</Button>
                         </ButtonGroup>
-                        <DialogContentText>
-                            Here we will display options for the plot we want to edit.
-                        </DialogContentText>
+                        <div className="plotMenuGridContainer">
+                            <div className="plotMenuGridTitleWrapper">
+                                <div>Sensor</div>
+                                <div>Post-processing</div>
+                                <div>Color</div>
+                                <div></div>
+                            </div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>
+                                <RemoveIcon/>
+                            </div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>
+                                <RemoveIcon/>
+                            </div>
+                        </div>
 
                     </DialogContent>
                     <DialogActions>
