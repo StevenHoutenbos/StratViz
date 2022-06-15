7b908c50a54d344ef720d08ef8bd3247a3352aa2
Steven <steven@houtenbos.nl>
2022-06-15 14:11:40 +0200

    changed icon

--- client/src/components/PlotContent.js
+++ client/src/components/PlotContent.js
@@ -9,11 +9,11 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { ButtonGroup } from "@mui/material";
 import { teal } from "@mui/material/colors";
-import { TiPencil } from 'react-icons/ti';
+import EditIcon from '@mui/icons-material/Edit';
 
 const primary = teal[500];
 
-function PlotContent(props){
+function PlotContent(props) {
 
     const [open, setOpen] = React.useState(false);
     const [selectedBtn, setSelectedBtn] = React.useState(1);
@@ -21,41 +21,50 @@ function PlotContent(props){
     const activeColor = teal[500]
 
     const handleClickOpen = () => {
-      setOpen(true);
+        setOpen(true);
     };
-  
+
     const handleClose = () => {
-      setOpen(false);
+        setOpen(false);
     };
 
     return (
         <div class="plotContainer">
             <Plot
-                    data={[
-                      {
-                      x: [1, 2, 3, 4, 5, 6, 7],
-                      y: [2, 6, 3, 5, 1, 8, 3],
-                      type: 'scatter',
-                      mode: 'lines',
-                      marker: {color: 'red'},
+                data={[
+                    {
+                        x: [1, 2, 3, 4, 5, 6, 7],
+                        y: [2, 6, 3, 5, 1, 8, 3],
+                        type: 'scatter',
+                        mode: 'lines',
+                        marker: { color: 'red' },
+                    }
+                ]}
+                layout={{
+                    autosize: true,
+                    height: 200,
+                    useResizeHandler: true,
+                    autoExpand: true,
+                    className: "plotGraph",
+                    margin: {
+                        l: 50,
+                        r: 30,
+                        b: 50,
+                        t: 30,
+                        pad: 15
                       }
-                    ]}
-                    layout={ {
-                              autosize: true,
-                              useResizeHandler: true,
-                              className: "plotGraph"} }
-                    />
-                    <p>
-                <Button variant="outlined" onClick={handleClickOpen} class="editButton" style={{position: "relative", top: "-210px", borderRadius: "10px"}}>
-                    <TiPencil/>
+                }}
+            />
+                <Button variant="contained" onClick={handleClickOpen} className="editButton">
+                    <EditIcon/>
                 </Button>
                 <Dialog open={open} onClose={handleClose}>
                     <DialogTitle>{props.plotName}</DialogTitle>
                     <DialogContent>
                         <ButtonGroup disableElevation variant="contained" color="primary">
-                            <Button color={selectedBtn === 1 ?  "secondary" : "primary"} onClick={()=>setSelectedBtn(1)}>Lux</Button>
-                            <Button color={selectedBtn === 2 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(2)}>Stella</Button>
-                            <Button color={selectedBtn === 3 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(3)}>Vie</Button>
+                            <Button color={selectedBtn === 1 ? "secondary" : "primary"} onClick={() => setSelectedBtn(1)}>Lux</Button>
+                            <Button color={selectedBtn === 2 ? "secondary" : "primary"} onClick={() => setSelectedBtn(2)}>Stella</Button>
+                            <Button color={selectedBtn === 3 ? "secondary" : "primary"} onClick={() => setSelectedBtn(3)}>Vie</Button>
                         </ButtonGroup>
                         <DialogContentText>
                             Here we will display options for the plot we want to edit.
@@ -63,13 +72,12 @@ function PlotContent(props){
 
                     </DialogContent>
                     <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Save</Button>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleClose}>Save</Button>
                     </DialogActions>
                 </Dialog>
-            </p>
         </div>
-        
+
     )
 }
 
