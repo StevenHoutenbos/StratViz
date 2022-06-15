3994cd95e79c499aacc75ea05f24192dd1532041
Steven <steven@houtenbos.nl>
2022-06-15 13:12:55 +0200

    added data from messages.json, now displays all non-continuous sensors in grio

--- client/src/components/NonContinuous.js
+++ client/src/components/NonContinuous.js
@@ -1,18 +1,41 @@
 import React from "react";
-import nonContinuousValues from '../nonContinuousValues.json'
+import nonContinuousValues from '../messages.json'
 
 function NonContinuous(props) {
-    return(
+    return (
         <div className="card">
             <h1>Non-continuous Values</h1>
             <div className="ncGridContainer">
-            {nonContinuousValues.signals.map((signal) => {return(
-                <>
-                    <div><p>{signal.signalName}</p></div>
-                    <div><p>{signal.value}</p></div>
-                </>
-            )
-            })}
+                {nonContinuousValues.map((signal) => {
+                    if (!(signal["Data types"].includes("int") || signal["Data types"].includes("float"))) {
+
+                        const dataTypesArr = signal["Data types"].split(',');
+                        const fieldNamesArr = signal["Field names"].split(',');
+                        
+                        const jsxArr = [];
+
+                        if (dataTypesArr.length != fieldNamesArr.length) {
+                            console.log('Arrays not of same size!')
+                        } else {
+                            for (let i = 0; i < dataTypesArr.length; i++) {
+                                if (!dataTypesArr[i].includes("int") || !dataTypesArr[i].includes("float")) {
+                                    jsxArr.push(
+                                        <>
+                                            <div><p className="signalName">{(i == 0) ? signal.Name : ""}</p></div>
+                                            <div><p>{fieldNamesArr[i]}</p></div>
+                                            <div><p>{dataTypesArr[i]}</p></div>
+                                        </>
+                                    )
+                                }
+                            }
+
+                            return(
+                                <> {jsxArr} </>
+                            )
+                        }
+                    }
+                }
+                )}
             </div>
         </div>
     )
