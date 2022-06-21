a4c43af28bab5f4d3054645e69531783172fafb0
Steven <steven@houtenbos.nl>
2022-06-21 12:55:02 +0200

    Added back plot menu. Still needs styling

--- client/src/components/PlotContent.js
+++ client/src/components/PlotContent.js
@@ -10,6 +10,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { ButtonGroup } from "@mui/material";
 import { teal } from "@mui/material/colors";
 import EditIcon from '@mui/icons-material/Edit';
+import RemoveIcon from '@mui/icons-material/Remove'
 //import * as socketData from '../index.js';
 
 const primary = teal[500];
@@ -134,22 +135,45 @@ function PlotContent(props) {
                 <Button variant="contained" onClick={handleClickOpen} className="editButton">
                     <EditIcon/>
                 </Button>
-                <Dialog open={open} onClose={handleClose}>
+                <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth={"md"}>
                     <DialogTitle>{props.plotName}</DialogTitle>
                     <DialogContent>
                         <ButtonGroup disableElevation variant="contained" color="primary">
-                            <Button color={selectedBtn === 1 ? "secondary" : "primary"} onClick={() => setSelectedBtn(1)}>Lux</Button>
-                            <Button color={selectedBtn === 2 ? "secondary" : "primary"} onClick={() => setSelectedBtn(2)}>Stella</Button>
-                            <Button color={selectedBtn === 3 ? "secondary" : "primary"} onClick={() => setSelectedBtn(3)}>Vie</Button>
+                            <Button color={selectedBtn === 1 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(1)}>Lux</Button>
+                            <Button color={selectedBtn === 2 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(2)}>Stella</Button>
+                            <Button color={selectedBtn === 3 ? "secondary" : "primary"} onClick={()=>setSelectedBtn(3)}>Vie</Button>
                         </ButtonGroup>
-                        <DialogContentText>
-                            Here we will display options for the plot we want to edit.
-                        </DialogContentText>
-
+                        <div className="plotMenuGridContainer">
+                            <div className="plotMenuGridTitleWrapper">
+                                <div>Sensor</div>
+                                <div>Post-processing</div>
+                                <div>Color</div>
+                                <div></div>
+                            </div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>
+                                <RemoveIcon/>
+                            </div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>Dit is tekst</div>
+                            <div>
+                                <RemoveIcon/>
+                            </div>
+                        </div>
+                        <TextField
+                        fullWidth
+                        id="filled-search"
+                        label="Search field"
+                        type="search"
+                        sx={{marginTop: "20px"}}
+                        />
                     </DialogContent>
                     <DialogActions>
-                        <Button onClick={handleClose}>Cancel</Button>
-                        <Button onClick={handleClose}>Save</Button>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Save</Button>
                     </DialogActions>
                 </Dialog>
         </div>
