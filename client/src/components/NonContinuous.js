3b92f3e69fdcc3f9b1cdb9c4ac20bd22e6579fae
DanielVanRijswijk <49940578+DanielVanRijswijk@users.noreply.github.com>
2022-06-25 17:36:03 +0200

    Now 'properly' shows real-time data in both the graphs and non-continuous page
    Todo:
    - Remove subscription model, doesn't work and seems unnecessary
    - Fix timestamps in plot
    - Maybe add timestamps to non-continous as well to show last update

--- client/src/components/NonContinuous.js
+++ client/src/components/NonContinuous.js
@@ -1,37 +1,83 @@
-import React from "react";
-import nonContinuousValues from '../messages.json'
+import React, { useEffect, useState } from "react";
+import signals from '../messages.json'
 import * as sio from "../index"
 
 function NonContinuous(props) {
+    const [lastVals, setLastVals] = useState({});
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        sio.socket.on("dataevent", (data) => { // TODO: change to real-time only
+            setData((curData) => [...curData, data]);
+        })
+        return function cleanup() {
+            sio.socket.off("dataevent");
+        };
+    }, [])
+
+    useEffect(() => {
+        if (data.length == 0) return;
+        // sdata = { topic: topic, key: car, data: {data object}}
+        var sdata = data[0];
+
+        console.log(sdata);
+        var newVal = {};
+
+        // For all the fields in the data object
+        for (const [key, value] of Object.entries(sdata.data)) {
+            // That are not timestamp or name
+            if (key === "timestamp" || key === "name") {
+                continue;
+            }
+
+            // Add it to the newVal object
+            newVal[key] = sdata.data[key].toString();
+        }
+
+        console.log(newVal);
+        var newVals = structuredClone(lastVals);
+        // Then set the value object of this topic to the new value
+        newVals[sdata.topic] = newVal;
+
+        // Update the state
+        setLastVals(newVals);
+
+        // Remove sdata
+        data.shift();
+        setData(data);
+    }, [data]);
+
     return (
         <div className="card">
             <h1>Non-continuous Values</h1>
             <div className="ncGridContainer">
-                {nonContinuousValues.map((signal) => {
+                {signals.map((signal) => {
                     if (!(signal["Data types"].includes("int") || signal["Data types"].includes("float"))) {
 
-                        const dataTypesArr = signal["Data types"].split(',');
-                        const fieldNamesArr = signal["Field names"].split(',');
-                        
+                        const unitsArr = signal["Units"].replaceAll(" ", "").split(',');
+                        const fieldNamesArr = signal["Field names"].replaceAll(" ", "").split(',');
+
                         const jsxArr = [];
 
-                        if (dataTypesArr.length != fieldNamesArr.length) {
+                        if (unitsArr.length != fieldNamesArr.length) {
                             console.log('Arrays not of same size!')
                         } else {
-                            for (let i = 0; i < dataTypesArr.length; i++) {
-                                if (!dataTypesArr[i].includes("int") || !dataTypesArr[i].includes("float")) {
-                                    sio.subscribe(signal.Name.split(" - ")[0], "car0");
-                                    jsxArr.push(
-                                        <>
-                                            <div><p className="signalName">{(i == 0) ? signal.Name : ""}</p></div>
-                                            <div><p>{fieldNamesArr[i]}</p></div>
-                                            <div><p>{dataTypesArr[i]}</p></div>
-                                        </>
-                                    )
-                                }
+                            for (let i = 0; i < fieldNamesArr.length; i++) {
+                                console.log(fieldNamesArr[i]);
+                                console.log(lastVals[signal.Name]);
+
+                                jsxArr.push(
+                                    <>
+                                        <div><p className="signalName">{(i == 0) ? signal.Name : ""}</p></div>
+                                        <div><p>{fieldNamesArr[i]}</p></div>
+                                        <div><p>{lastVals[signal.Name] == undefined ? "-" : lastVals[signal.Name][fieldNamesArr[i]]}</p></div>
+                                        <div><p>{unitsArr[i]}</p></div>
+                                    </>
+                                )
+
                             }
 
-                            return(
+                            return (
                                 <> {jsxArr} </>
                             )
                         }
